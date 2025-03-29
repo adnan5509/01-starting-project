c6fadd2e5ef37163fc255c7f1ab52be4ba7b84bd
adnan5509 <adnanafzal5509@gmail.com>
2025-03-29 02:54:23 +0100

    Format task due date to display as full date in user task component

--- src/app/components/user-task/user-task.component.ts
+++ src/app/components/user-task/user-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../model/task.model';
 import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './user-task.component.html',
   styleUrl: './user-task.component.css'
 })
